774c0fdf5ca1fb83603b34b989bea35bf114894c
prajna7jain <mrprajnajain@gmail.com>
2025-08-29 14:04:10 +0530

    edited app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_weather():
 
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+    app.run(debug=True)
 
 # if __name__ == "__main__":
 #     app.run(debug=True, host="127.0.0.1", port=8000)
